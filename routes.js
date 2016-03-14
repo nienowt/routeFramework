f6457f24127093c93c6a5138f21e5f5888c1c1fa
Thom Nienow <elstad.nienow@gmail.com>
2016-03-13 20:49:33 -0700

    moved basic filesaving/ammending into routes

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@
 
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var Router = module.exports = function(){
   this.routes = {
@@ -36,7 +37,7 @@ Router.prototype.route = function(){
     if (this.routes[req.method][req.url]){
       routeFunction = this.routes[req.method][req.url];
       routeFunction(req, res);
-      
+
     } else if (this.routes[req.method][dirname + ':id']) {
       routeFunction = this.routes[req.method][dirname +':id'];
       routeFunction(req, res);
@@ -49,8 +50,78 @@ Router.prototype.route = function(){
   };
 };
 
+
 Router.prototype.start = function(port){
   http.createServer(this.route()).listen(port, () => {
     console.log('live ' + port);
+  });
+};
+
+Router.prototype.basicPost = function(route, dirName, contentType){
+  this.post(route, (req, res) => {
+    console.log(route + ' post route hit');
+    res.writeHead(200, {'content-type':contentType});
+    req.on('data', (data) => {
+      var info = data.toString();
+      fs.mkdir(__dirname + '/' + dirName, () => {
+        fs.readdir('./' + dirName,(err, files) => {
+          if (err) return err;
+          var fileId = function(){
+            var count = files.length;
+            files.forEach((file)=>{
+              if (file === files.length + '.json'){
+                count++;
+              }
+            });
+            return count;
+          };
+          fs.writeFile('./' + dirName + '/' + fileId() + '.json', info, () =>{
+            console.log('File saved');
+          });
+        });
+      });
+      req.on('end', () =>{
+        return res.end();
+      });
+    });
+  });
+};
+
+Router.prototype.basicPut = function(route, dirName, contentType){
+  this.put(route + '/:id', (req, res) => {
+    console.log('put hit');
+    var fileId = path.basename(req.url) + '.json';
+
+    res.writeHead(200,{'content-type':contentType});
+    req.on('data', (data) => {
+      fs.readFile('./'+ dirName +'/'+ fileId, (err, fileData) => {
+        if(!fileData) return res.end(console.log('File does not exist')); //won't just write new file
+        var cur = JSON.parse(fileData.toString());
+        var change = JSON.parse(data.toString());
+        var info = data.toString();
+
+        if (cur.type === change.type){ //won't write if no change
+          console.log('File Not Changed');
+          return res.end();
+        }
+        fs.writeFile('./' + dirName + '/' + fileId, info, () => {
+          console.log('File Changed');
+        });
+        return res.end();
+      });
+    });
+  });
+};
+
+Router.prototype.basicIdDel = function(route, dirName){
+  this.delete(route + '/:id', (req, res) => {
+    var fileId = path.basename(req.url) + '.json';
+    res.writeHead(200,{'content-type':'text/html'});
+    fs.unlink('./' + dirName + '/' + fileId, () =>{
+      if(!fileId) return res.end(console.log('File does not exist'));
+
+      console.log('del hit');
+      return res.end();
+    });
   });
 };
